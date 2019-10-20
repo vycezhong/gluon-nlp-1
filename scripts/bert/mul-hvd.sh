0149a73e523721724a58eb25e6329cb13484019e
root <root@ip-172-31-47-42.ec2.internal>
2019-10-20 00:30:38 +0000

    +clush

--- scripts/bert/mul-hvd.sh
+++ scripts/bert/mul-hvd.sh
@@ -1,15 +1,4 @@
-#mpirun -np 2 --hostfile hosts -display-allocation -display-map --allow-run-as-root -mca pml ob1 -mca btl ^openib \
-#            -mca btl_tcp_if_exclude docker0,lo --map-by ppr:4:socket:PE=4 \
-#            --mca plm_rsh_agent 'ssh -q -o StrictHostKeyChecking=no -p 12340' \
-#            -x NCCL_MIN_NRINGS=8 -x NCCL_DEBUG=INFO \
-#            -x HOROVOD_HIERARCHICAL_ALLREDUCE=1 \
-#            -x HOROVOD_CYCLE_TIME=1 \
-#            -x MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN=120 \
-#            -x MXNET_SAFE_ACCUMULATION=1 \
-#	    --tag-output python -c 'import mxnet as mx; import horovod.mxnet as hvd; print(mx); hvd.init(); import socket; print(socket.gethostname())'
-
-
-# mpirun -np 2 --hostfile hosts -display-allocation --allow-run-as-root -mca btl ^openib -mca btl_tcp_if_exclude docker0,lo -x LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda/compat/lib:/usr/local/nvidia/lib:/usr/local/nvidia/lib64:/usr/local/lib --mca plm_rsh_agent 'ssh -q -o StrictHostKeyChecking=no -p 12340' --tag-output python -c 'import os; print(os.environ); import socket; print(socket.gethostname()); import mxnet as mx; import horovod.mxnet as hvd; print(mx); hvd.init(); print(hvd.rank())'
+# python -c 'import os; print(os.environ); import socket; print(socket.gethostname()); import mxnet as mx; import horovod.mxnet as hvd; print(mx); hvd.init(); print(hvd.rank())'
 
 	    #--map-by ppr:4:socket:PE=4 \
 	    #--synthetic_data --eval_use_npz \
@@ -46,7 +35,10 @@ pkill python
 #	    --verbose \
 #	    --comm_backend horovod --log_interval $LOGINTERVAL
 
-mpirun -np $NP --hostfile hosts -display-allocation --allow-run-as-root \
+	    #--synthetic_data --eval_use_npz \
+	    #--verbose \
+
+mpirun -np $NP --hostfile $HOST -display-allocation --allow-run-as-root \
 	    -mca btl ^openib -mca btl_tcp_if_exclude docker0,lo \
             --bind-to none \
 	    --mca plm_rsh_agent "ssh -q -o StrictHostKeyChecking=no -p $PORT" \
@@ -64,7 +56,6 @@ mpirun -np $NP --hostfile hosts -display-allocation --allow-run-as-root \
 	    --ckpt_interval $CKPTINTERVAL \
 	    --dtype $DTYPE \
 	    --ckpt_dir $CKPTDIR \
-	    --synthetic_data --eval_use_npz \
 	    --lr $LR \
 	    --total_batch_size $BS \
 	    --total_batch_size_eval $BS \
@@ -74,5 +65,4 @@ mpirun -np $NP --hostfile hosts -display-allocation --allow-run-as-root \
 	    --max_predictions_per_seq $MAX_PREDICTIONS_PER_SEQ \
 	    --num_data_workers 4 \
 	    --no_compute_acc --raw \
-	    --verbose \
-	    --comm_backend horovod --log_interval $LOGINTERVAL
+	    --comm_backend horovod --log_interval $LOGINTERVAL $OPTIONS
