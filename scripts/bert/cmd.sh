81a7b1c91e3f712a737cecdde8bca5e939158e70
root <root@ip-172-31-15-227.us-west-2.compute.internal>
2019-10-20 21:43:05 +0000

    fix missing env var

--- scripts/bert/cmd.sh
+++ scripts/bert/cmd.sh
@@ -1,6 +1,4 @@
-#TRUNCATE_NORM=1 LAMB_BULK=30 EPS_AFTER_SQRT=1 NUMSTEPS=900000 DTYPE=float16 BS=256 ACC=2 MODEL=bert_24_1024_16 MAX_SEQ_LENGTH=128 MAX_PREDICTIONS_PER_SEQ=20 LR=0.0001 LOGINTERVAL=1 CKPTDIR=ckpt_stage1_adam_1x_kv CKPTINTERVAL=300000 OPTIMIZER=bertadam WARMUP_RATIO=0.0001 bash kvstore.sh
-
-export DEBUG=1
+export DEBUG=0
 export HOST=hosts_32
 export OTHER_HOST=hosts_31
 export DOCKER_IMAGE=haibinlin/worker_mxnet:c5fd6fc-1.5-cu90-79e6e8-79e6e8
@@ -11,14 +9,14 @@ export LAMB_BULK=30
 export EPS_AFTER_SQRT=1
 export DTYPE=float16
 export MODEL=bert_24_1024_16
-export CKPTDIR=/bert/ckpts/debug
+export CKPTDIR=/bert/ckpts/stage1_64k_32k_base
 export CKPTINTERVAL=300000000
 export OPTIMIZER=lamb2
-export COMMIT=91ecec6
 export CLUSHUSER=ec2-user
 export NO_SHARD=1
 export HIERARCHICAL=1
 export EVALINTERVAL=1000
+export COMMIT=58435d04
 
 export PORT=12451
 sleep 5
@@ -32,8 +30,8 @@ else
     export NUMSTEPS=7813
     #export NUMSTEPS=15625
 fi
-#bash clush-hvd.sh
-#BS=65536 ACC=4 MAX_SEQ_LENGTH=128 MAX_PREDICTIONS_PER_SEQ=20 LR=0.006 WARMUP_RATIO=0.2843 bash mul-hvd.sh
+bash clush-hvd.sh
+BS=65536 ACC=4 MAX_SEQ_LENGTH=128 MAX_PREDICTIONS_PER_SEQ=20 LR=0.006 WARMUP_RATIO=0.2843 bash mul-hvd.sh
 #BS=32768 ACC=8 MAX_SEQ_LENGTH=128 MAX_PREDICTIONS_PER_SEQ=20 LR=0.005 WARMUP_RATIO=0.2 bash mul-hvd.sh
 
 export PORT=12452
--- scripts/bert/mul-hvd.sh
+++ scripts/bert/mul-hvd.sh
@@ -47,6 +47,11 @@ mpirun -np $NP --hostfile $HOST -display-allocation --allow-run-as-root \
 	    -x HOROVOD_CYCLE_TIME=30 \
 	    -x MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN_FWD=120 \
 	    -x MXNET_SAFE_ACCUMULATION=1 \
+            -x NCCL_IB_DISABLE=1 \
+            -x TRUNCATE_NORM=$TRUNCATE_NORM \
+            -x LAMB_BULK=$LAMB_BULK \
+            -x EPS_AFTER_SQRT=$EPS_AFTER_SQRT \
+            -x NO_SHARD=$NO_SHARD \
 	    --tag-output ompi_bind_DGX1.sh python3 run_pretraining.py \
 	    --data='/data/book-corpus/book-corpus-large-split/*.train,/data/enwiki/enwiki-feb-doc-split/*.train' \
 	    --data_eval='/data/book-corpus/book-corpus-large-split/*.test,/data/enwiki/enwiki-feb-doc-split/*.test' \
