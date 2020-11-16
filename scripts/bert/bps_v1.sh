2b0df0191697af65f1e43f7cec9dfb2a88583d80
zhongyuchen <izhongyuchen@gmail.com>
2020-11-16 12:31:25 +0800

    update

--- scripts/bert/bps_v1.sh
+++ scripts/bert/bps_v1.sh
@@ -17,14 +17,14 @@ LR=0.001768
 WARMUP_RATIO=0.025
 CONST_RATIO=0
 NUMSTEPS=112500
-OPTIMIZER=bertadam
+OPTIMIZER=lamb
 
 MAX_SEQ_LENGTH=128
 MAX_PREDICTIONS_PER_SEQ=20
 SHORT_SEQ_PROB=0.1
 
 LOGINTERVAL=10
-CKPTDIR=$HOME/checkpoints/gluon-nlp-1/ckpt_stage1_ds_bertadam_4k_bps_sz
+CKPTDIR=$HOME/checkpoints/gluon-nlp-1/ckpt_stage1_ds_lamb_topkef_4k_bps_sz
 CKPTINTERVAL=5000
 
 DATA_HOME=$HOME/datasets/bert/pretrain/book-wiki-split-2k-v3
@@ -80,6 +80,9 @@ cmd="python3 /home/ubuntu/repos/byteps/launcher/dist_launcher.py \
   --dataset_cached \
   --num_max_dataset_cached 4 \
   --short_seq_prob $SHORT_SEQ_PROB \
+  --compressor topk \
+  --k 0.01 \
+  --ef vanilla \
   --comm_backend byteps --log_interval $LOGINTERVAL --raw\""
 
 echo $cmd
